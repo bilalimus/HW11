03b8ae996cd41bec92831969af9adb7712f46012
Maksat <bilalimus@gmail.com>
2022-08-09 01:14:28 +0600

    start project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-
+import { useState } from 'react';
+import styled from 'styled-components'
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('green')
+
+  
+  
   return (
     <div className="App">
-      
+      <Square bg={color}/>
     </div>
   );
 }
 
 export default App;
+
+const Square = styled.div`
+width: 400px;
+height: 400px;
+border: 2px solid black;
+background-color: ${props => props.color};
+`
