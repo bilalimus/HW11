e10dc15245c3bea911eeadbc3a34aac5cc3c22b2
Maksat <bilalimus@gmail.com>
2022-08-09 02:06:03 +0600

    HW11 is DONE

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { useState } from 'react';
-import styled from 'styled-components'
-import './App.css';
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import "./App.css";
 
 function App() {
-  const [color, setColor] = useState('green')
+  const [color, setColor] = useState("");
+  const [state, setState] = useState(true);
 
-  
-  
+  useEffect(() => {
+    setTimeout(() => {
+      if (state) {
+        setColor("red");
+      } else {
+        setColor("green");
+      }
+      setState(!state);
+    }, 1000);
+  }, [state]);
+
   return (
     <div className="App">
-      <Square bg={color}/>
+      <Square backgroundColor={color} />
     </div>
   );
 }
@@ -17,8 +27,8 @@ function App() {
 export default App;
 
 const Square = styled.div`
-width: 400px;
-height: 400px;
-border: 2px solid black;
-background-color: ${props => props.color};
-`
+  width: 400px;
+  height: 400px;
+  border: 2px solid black;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+`;
